486dd868f9fb4857963950ccde13bb38be8e21c0
Adam Kulas <adam.kulas1@gmail.com>
2018-09-15 14:06:14 -0400

    built factor tree using recursion

--- problem3.py
+++ problem3.py
@@ -4,28 +4,17 @@ def isprime(num):
             return False
     return True
 
-def primefactorizer(num):
-    for i in range(2,num):
-        if isprime(i):
-            if num % i == 0:
-                primefactors.add(int(i))
-                primefactorizer(int(num/i))
-    return num
-
 def factorizer(num):
     for i in range(2,num):
         if num % i == 0:
-            regfactors.append(int(i))
+            factors.append(int(i))
             print(int(num/i), i)
             factorizer(int(num/i))
             return i
-    return regfactors.append(int(num))
+    return factors.append(int(num))
 
-num1 = 40575
+num1 = 13195
 num2 = 600851475143
-regfactors = []
+factors = []
 factorizer(num1)
-print(sorted(set(regfactors)))
-primefactors = set([])
-primefactorizer(num1)
-print(sorted(primefactors))
+print(sorted(set(factors)))
