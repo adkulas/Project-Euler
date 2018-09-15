7a044c26a5381f213765f828171d221eaf3e258a
Adam Kulas <adam.kulas1@gmail.com>
2018-09-15 17:49:49 -0400

    added problem statement

--- problem5.py
+++ problem5.py
@@ -1,3 +1,9 @@
+'''
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+'''
+
 def check_divisible(num, divisor_list):
     for i in divisor_list:
         if num % i != 0:
@@ -14,4 +20,3 @@ def lowest_common_multiple(top_num):
 
 if __name__ == '__main__':
     print(lowest_common_multiple(20))
-    
