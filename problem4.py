e3b246e9b81164af9135e89b7f0b06d3e45ee88a
Adam Kulas <adam.kulas1@gmail.com>
2018-09-15 15:42:03 -0400

    first attempt

---
+++ problem4.py
@@ -0,0 +1,20 @@
+def ispalendrome(num):
+    if str(num) == str(num)[::-1]:
+        return True
+    else:
+        return False
+
+def largest_palendrome(digits):
+    max_num = 10**(digits)-1
+    largest = 0
+    for i in range(max_num, 1, -1):
+        for j in range(max_num, 1, -1):
+            print(i, j)
+            if ispalendrome(i*j):
+                if i*j > largest:
+                    largest = i*j
+    return largest
+
+if __name__ == '__main__':
+    i, j = largest_palendrome(3)
+    print(i,j, i*j)
